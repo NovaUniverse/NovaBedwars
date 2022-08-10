d787b38a6e5228fd7381696474c26affcdcd76d7
Anton udén <udenanton@hotmail.com>
2022-08-10 16:54:13 +0200

    Plugin load logic

--- src/main/java/net/novauniverse/bedwars/NovaBedwars.java
+++ src/main/java/net/novauniverse/bedwars/NovaBedwars.java
@@ -1,32 +1,134 @@
 package net.novauniverse.bedwars;
 
+import net.novauniverse.bedwars.game.Bedwars;
+import net.novauniverse.bedwars.game.config.BedwarsConfig;
 import net.novauniverse.bedwars.game.holder.ItemShopHolder;
 import net.novauniverse.bedwars.game.holder.UpgradeShopHolder;
+import net.zeeraa.novacore.commons.log.Log;
+import net.zeeraa.novacore.commons.utils.JSONFileUtils;
+import net.zeeraa.novacore.spigot.gameengine.module.modules.game.GameManager;
+import net.zeeraa.novacore.spigot.gameengine.module.modules.game.map.mapmodule.MapModuleManager;
+import net.zeeraa.novacore.spigot.gameengine.module.modules.game.mapselector.selectors.RandomMapSelector;
+import net.zeeraa.novacore.spigot.gameengine.module.modules.game.mapselector.selectors.guivoteselector.GUIMapVote;
+import net.zeeraa.novacore.spigot.gameengine.module.modules.gamelobby.GameLobby;
+import net.zeeraa.novacore.spigot.module.ModuleManager;
+import net.zeeraa.novacore.spigot.module.modules.compass.CompassTracker;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public final class NovaBedwars extends JavaPlugin implements Listener {
-    private static NovaBedwars instance;
+	private static NovaBedwars instance;
 
-    public static NovaBedwars getInstance() {
-        return instance;
-    }
-    @Override
-    public void onEnable() {
-        instance = this;
-        // Plugin startup logic
+	private Bedwars game;
+	private boolean disableDefaultEndSound;
 
-    }
+	public static NovaBedwars getInstance() {
+		return instance;
+	}
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
-    }
-    @EventHandler
-    public void ItemClick(InventoryClickEvent e) {
-        if (e.getClickedInventory().getHolder() instanceof ItemShopHolder || e.getClickedInventory().getHolder() instanceof UpgradeShopHolder) {
-        }
-    }
+	public boolean isDisableDefaultEndSound() {
+		return disableDefaultEndSound;
+	}
+
+	public void setDisableDefaultEndSound(boolean disableDefaultEndSound) {
+		this.disableDefaultEndSound = disableDefaultEndSound;
+	}
+
+	public Bedwars getGame() {
+		return game;
+	}
+
+	@Override
+	public void onEnable() {
+		NovaBedwars.instance = this;
+		saveDefaultConfig();
+
+		disableDefaultEndSound = getConfig().getBoolean("disable_default_end_sound");
+		boolean combatTagging = getConfig().getBoolean("combat_tagging");
+		boolean disableNovaCoreGameLobby = getConfig().getBoolean("disable_novacore_gamelobby");
+
+		File mapFolder = new File(this.getDataFolder().getPath() + File.separator + "Maps");
+		File worldFolder = new File(this.getDataFolder().getPath() + File.separator + "Worlds");
+
+		File mapOverrides = new File(this.getDataFolder().getPath() + File.separator + "map_overrides.json");
+		if (mapOverrides.exists()) {
+			Log.info(getName(), "Trying to read map overrides file");
+			try {
+				JSONObject mapFiles = JSONFileUtils.readJSONObjectFromFile(mapOverrides);
+
+				boolean relative = mapFiles.getBoolean("relative");
+
+				mapFolder = new File((relative ? this.getDataFolder().getPath() + File.separator : "") + mapFiles.getString("maps_folder"));
+				worldFolder = new File((relative ? this.getDataFolder().getPath() + File.separator : "") + mapFiles.getString("worlds_folder"));
+
+				Log.info(getName(), "New paths:");
+				Log.info(getName(), "Map folder: " + mapFolder.getAbsolutePath());
+				Log.info(getName(), "World folder: " + worldFolder.getAbsolutePath());
+			} catch (JSONException | IOException e) {
+				e.printStackTrace();
+				Log.error(getName(), "Failed to read map overrides from file " + mapOverrides.getAbsolutePath());
+			}
+		}
+
+		try {
+			FileUtils.forceMkdir(getDataFolder());
+			FileUtils.forceMkdir(mapFolder);
+			FileUtils.forceMkdir(worldFolder);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			Log.fatal(getName(), "Failed to setup data directory");
+			Bukkit.getPluginManager().disablePlugin(this);
+			return;
+		}
+
+		ModuleManager.enable(GameManager.class);
+		ModuleManager.enable(CompassTracker.class);
+		if (!disableNovaCoreGameLobby) {
+			ModuleManager.enable(GameLobby.class);
+		}
+
+		MapModuleManager.addMapModule("bedwars.config", BedwarsConfig.class);
+
+		GameManager.getInstance().setUseCombatTagging(combatTagging);
+
+		Bukkit.getPluginManager().registerEvents(this, this);
+
+		this.game = new Bedwars();
+		GameManager.getInstance().loadGame(game);
+
+		if (!disableNovaCoreGameLobby) {
+			GUIMapVote mapSelector = new GUIMapVote();
+			GameManager.getInstance().setMapSelector(mapSelector);
+			Bukkit.getServer().getPluginManager().registerEvents(mapSelector, this);
+		} else {
+			GameManager.getInstance().setMapSelector(new RandomMapSelector());
+		}
+
+		Log.info(getName(), "Loading maps from " + mapFolder.getPath());
+		GameManager.getInstance().readMapsFromFolder(mapFolder, worldFolder);
+	}
+
+	@Override
+	public void onDisable() {
+		Bukkit.getScheduler().cancelTasks(this);
+		HandlerList.unregisterAll((Plugin) this);
+	}
+
+	@EventHandler
+	public void ItemClick(InventoryClickEvent e) {
+		if (e.getClickedInventory().getHolder() instanceof ItemShopHolder || e.getClickedInventory().getHolder() instanceof UpgradeShopHolder) {
+		}
+	}
 }
--- src/main/java/net/novauniverse/bedwars/game/config/BedwarsConfig.java
+++ src/main/java/net/novauniverse/bedwars/game/config/BedwarsConfig.java
@@ -8,28 +8,31 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BedwarsConfig extends MapModule {
+	private int upgradeTime;
+	private int generatorSpeed;
+	private List<ConfiguredBaseData> bases;
 
-    private int upgradeTime;
-    private int generatorSpeed;
-    private List<ConfiguredBaseData> bases;
-    public BedwarsConfig(JSONObject json) {
-        super(json);
+	public BedwarsConfig(JSONObject json) {
+		super(json);
 
-        this.upgradeTime = json.getInt("upgrade_time");
-        this.generatorSpeed = json.getInt("generator_speed");
-        this.bases = new ArrayList<>();
-        JSONArray basesJson = json.getJSONArray("bases");
-        for (int i = 0; i < basesJson.length(); i++) {
-            bases.add(new ConfiguredBaseData(basesJson.getJSONObject(i)));
-        }
-    }
-    public int getUpgradeTime() {
-        return upgradeTime;
-    }
-    public int getGeneratorSpeed() {
-        return generatorSpeed;
-    }
-    public List<ConfiguredBaseData> getBases() {
-        return bases;
-    }
+		this.upgradeTime = json.getInt("upgrade_time");
+		this.generatorSpeed = json.getInt("generator_speed");
+		this.bases = new ArrayList<>();
+		JSONArray basesJson = json.getJSONArray("bases");
+		for (int i = 0; i < basesJson.length(); i++) {
+			bases.add(new ConfiguredBaseData(basesJson.getJSONObject(i)));
+		}
+	}
+
+	public int getUpgradeTime() {
+		return upgradeTime;
+	}
+
+	public int getGeneratorSpeed() {
+		return generatorSpeed;
+	}
+
+	public List<ConfiguredBaseData> getBases() {
+		return bases;
+	}
 }
